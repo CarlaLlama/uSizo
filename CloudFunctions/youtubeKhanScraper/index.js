1992f981a83e811fa3abb3011a6d9cbedfcb5569
Carla Wilby <carla@zelda.org.za>
2019-08-04 00:40:39 +0200

    Added scrape logic

---
+++ CloudFunctions/youtubeKhanScraper/index.js
@@ -0,0 +1,30 @@
+let search = require('scrape-youtube');
+const request = require('request');
+const cheerio = require('cheerio');
+
+
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic 'scrape'
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.startScrape = (event, context) => {
+    const topicToScrape = event.data;
+    console.log(Buffer.from(topicToScrape, 'base64').toString());
+
+    search(`Khan Academy ${topicToScrape}`, {
+        limit : 5,
+        type : "video"
+    }).then(function(results){
+        console.log(results);
+        results.forEach(result => {
+            console.log(`Title: ${result.title} , link: ${result.link}`);
+        });
+    }, function(err){
+        console.log(err);
+    });
+};
+
+
