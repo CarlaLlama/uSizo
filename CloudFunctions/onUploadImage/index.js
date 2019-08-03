b28c48c32cf1951a992e981cf8cb9cfe5985b8b2
Carla Wilby <carla@zelda.org.za>
2019-08-03 12:42:35 +0200

    Added the Vision API init code

--- CloudFunctions/onUploadImage/index.js
+++ CloudFunctions/onUploadImage/index.js
@@ -1,3 +1,11 @@
+// Imports the Google Cloud client libraries
+const vision = require('@google-cloud/vision');
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+/* TODO: Remove the following and replace them with Environment Variables" */
+const bucketName = 'usizo-images';
+
 /**
  * Triggered from a change to a Cloud Storage bucket.
  *
@@ -5,6 +13,17 @@
  * @param {!Object} context Metadata for the event.
  */
 exports.onImageUploadToStorage = (event, context) => {
-    const gcsEvent = event;
-    console.log(`Processing file: ${gcsEvent.name}`);
+    console.log(`Processing file: ${event.name}`);
+
+    // Path to uploaded image is event.name
+    const fileName = event.name;
+
+    // Read a remote image as a text document
+    const [result] = await client.documentTextDetection(
+        `gs://${bucketName}/${fileName}`
+    );
+
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log(fullTextAnnotation.text);
+
 };
