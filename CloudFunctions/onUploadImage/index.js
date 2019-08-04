ad807ca6b2a0585f747d8c9be68ef456fae36ba1
Carla Wilby <carla@zelda.org.za>
2019-08-04 07:50:27 +0200

    Phelisa's wolframalpha integration

--- CloudFunctions/onUploadImage/index.js
+++ CloudFunctions/onUploadImage/index.js
@@ -1,10 +1,17 @@
 // Imports the Google Cloud client libraries
 const vision = require('@google-cloud/vision');
+//usizo answres bucket
+const Storage = require('@google-cloud/storage');
+const store = new Storage();
+const bucket = store.bucket('usizo-images');
+
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
 /* TODO: Remove the following and replace them with Environment Variables" */
 const bucketName = 'usizo-images';
+//wolfram api create client
+var wolfram = require('wolfram').createClient("H24G5X-8Y6X92H7JG")
 
 /**
  * Triggered from a change to a Cloud Storage bucket.
@@ -13,6 +20,7 @@ const bucketName = 'usizo-images';
  * @param {!Object} context Metadata for the event.
  */
 exports.onImageUploadToStorage = (event, context) => {
+
     console.log(`Processing file: ${event.name}`);
 
     // Path to uploaded image is event.name
@@ -24,10 +32,14 @@ exports.onImageUploadToStorage = (event, context) => {
             const fullTextAnnotation = result.fullTextAnnotation;
             if(fullTextAnnotation && fullTextAnnotation.hasOwnProperty('text')){
                 console.log(fullTextAnnotation.text);
+                postToWolfram(fullTextAnnotation.text);
+
+
             } else {
                 console.log('No text found.');
             }
             return;
+
         });
 };
 
@@ -37,4 +49,26 @@ async function readImage(fileName){
         `gs://${bucketName}/${fileName}`
     );
     return result;
+}
+
+function postToWolfram(result){
+    const options = {
+        destination: "/usizo-answers/answerimage"
+    };
+    let answer = wolfram.query(result, function(err, answer) {
+        if(err) throw err
+
+
+        console.log("Answer: %j", answer)
+        console.log("proccessed",answer[1].subpods);
+        console.log("proccessed",answer[1].subpods[0]);
+        console.log("proccessed",answer[1].subpods[0].image);
+
+        bucket.upload(''+answer[1].subpods[0].image, options).then(function(data) {
+            const file = data[0];
+        });
+
+        return answer;
+
+    });
 }
